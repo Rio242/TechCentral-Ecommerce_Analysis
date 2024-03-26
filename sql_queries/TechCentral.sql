036fd8659c6eb27911376b50815b2d907e5d3f41
Mario Strachan <96462826+Rio242@users.noreply.github.com>
2024-03-26 02:15:44 -0400

    Update TechCentral.sql
    Added stakeholder questions

--- sql_queries/TechCentral.sql
+++ sql_queries/TechCentral.sql
@@ -1,7 +1,11 @@
 --SQL scripts for comme
 
 --1.What are the quarterly trends for order count, sales, and AOV for Macbooks sold in North America across all years?
---
+--2.2) For products purchased in 2022 on the website or products purchased on mobile in any year, which region has the average highest time to deliver?
+--3.Are there certain products that are getting refunded more frequently than others?
+--4.Within each region, what is the most popular product? 
+--5. Which marketing channel has the highest average signup rate for the loyalty program, and how does this compare to the channel that has the highest number of loyalty program participants
+--Process: 
 
 select *
 from 
